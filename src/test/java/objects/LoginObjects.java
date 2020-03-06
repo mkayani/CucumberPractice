f5371ee0c64f7c4b4696d29783e0ddfa2abc56c5
mkayani <maryamkayani@gmail.com>
2020-03-06 09:31:11 -0600

    Updated Project

---
+++ src/test/java/objects/LoginObjects.java
@@ -0,0 +1,42 @@
+package objects;
+
+import dataProvider.LoginCredentials;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+public class LoginObjects {
+
+
+   // public static WebDriver driver;
+    public static LoginCredentials loginCredentials = new LoginCredentials();
+
+
+    @FindBy (id = "Log in")
+    private WebElement loginButton;
+
+    @FindBy (id = "email")
+    private WebElement emailSearchBox;
+
+    @FindBy (id = "password")
+    private WebElement passwordSearchBox;
+
+
+
+    public void loginButton(){
+        loginButton.click();
+    }
+
+    public void emailSearchBox(){
+        emailSearchBox.clear();
+        emailSearchBox.sendKeys(loginCredentials.username());
+    }
+
+    public void passwordSearchBox(){
+        passwordSearchBox.clear();
+        passwordSearchBox.sendKeys(loginCredentials.password());
+    }
+
+
+}
--- src/test/java/stepDefinition/Login.java
+++ src/test/java/stepDefinition/Login.java
@@ -2,6 +2,7 @@ package stepDefinition;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import dataProvider.LoginCredentials;
+import objects.LoginObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import cucumber.api.java.en.And;
@@ -10,27 +11,39 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
-public class Login {
-WebDriver driver;
-LoginCredentials loginCredentials = new LoginCredentials();
+public class Login extends LoginObjects {
+
+
+
+    public static WebDriver driver;
+    public static LoginCredentials loginCredentials = new LoginCredentials();
+
+
+    LoginObjects objOfLogin = PageFactory.initElements(driver, LoginObjects.class);
+
+
+
 
     @Before
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","C:\\Users\\marya\\IdeaProjects\\CucumberPractice\\src\\test\\java\\Resources\\chromedriver_win32 (3)\\chromedriver_win32 (4)\\chromedriver.exe");
-        this.driver = new ChromeDriver();
-        this.driver.manage().window().maximize();
-        this.driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
     }
+
 
     @After
     public void tearDown(){
-        this.driver.manage().deleteAllCookies();
-        this.driver.quit();
-        this.driver = null;
+        driver.manage().deleteAllCookies();
+        driver.quit();
+        driver = null;
     }
     @Given("^User navigates to stackoverflow website$")
     public void user_Navigates_To_Stackoverflow_Website() {
@@ -40,26 +53,20 @@ LoginCredentials loginCredentials = new LoginCredentials();
 
     @And("^User clicks on the login button on homepage$")
     public void user_clicks_on_the_login_button_on_homepage() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.findElement(By.partialLinkText("Log in")).click();
+        Thread.sleep(5000);
+       objOfLogin.loginButton();
     }
 
 
     @And("^User enters valid username$")
     public void user_enters_valid_username() {
-
-        WebElement email = driver.findElement(By.id("email"));
-        email.clear();
-        email.sendKeys(loginCredentials.username());
+        objOfLogin.emailSearchBox();
 
     }
 
     @And("^User enters valid password$")
     public void user_enters_valid_password() {
-        WebElement password = driver.findElement(By.id("password"));
-        password.clear();
-        password.sendKeys(loginCredentials.password());
-
+        objOfLogin.passwordSearchBox();
     }
 
     @When("^User clicks on the login button$")
@@ -70,7 +77,7 @@ LoginCredentials loginCredentials = new LoginCredentials();
 
     @Then("^the user should be taken to the successful login page$")
     public void the_user_should_be_taken_to_the_successful_login_page() throws InterruptedException {
-        Thread.sleep(3000);
+        Thread.sleep(5000);
         WebElement askQuestion = driver.findElement(By.partialLinkText("Ask Question"));
         Assert.assertTrue(askQuestion.isDisplayed());
 
